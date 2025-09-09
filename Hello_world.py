863bbb704ff62acbb9bbd19e6e976f5a8b53340c
GunterLover <20067net@naver.com>
2025-09-09 12:49:17 +0900

    Second commit

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,9 @@
-type = "wind"
+def print_hello():
+    print("Hello World!!")
+
+def print_KIT():
+    print("Kumoh National Institue of Technology.")
+
+if __name__ == '__main__':
+    print_KIT()
+    print_hello()
